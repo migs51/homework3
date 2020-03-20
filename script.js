2c2b17f5302ff9fe0511b7745e69d54ec591a9d3
Migs <mmejares@hotmail.com>
2020-03-19 22:56:40 -0500

    updated JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Array of special characters to be included in password
-let specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.']
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.']
 
 // Array of numeric characters to be included in password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
